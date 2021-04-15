470326420f1b9bb1b01e95485ee13983e979148e
vineeshayarlagadda <75362532+vineeshayarlagadda@users.noreply.github.com>
2021-04-15 22:26:58 +0530

    Update InfoOfTextExcercise5.java

--- InfoOfTextExcercise5.java
+++ InfoOfTextExcercise5.java
@@ -4,10 +4,10 @@ package day7.LAB3;
 public class InfoOfTextExcercise5 {
 	public static void main(String[] args) {
 		String input="Hey, how are you doing?\nI've been thinking about you.\rBy the way I'm waiting for our trip.";
-		int noOfCharacters=input.length();
+		int noOfCharacters=input.length(); //1st way to find no.of characters
 		
-		char charLength[]=input.toCharArray();
-		int noOfCharacters1=charLength.length;
+		char charLength[]=input.toCharArray();  
+		int noOfCharacters1=charLength.length;	//2nd way to find no.of characters
 		System.out.println("Number of characters : "+noOfCharacters1);
 		
 		String Lines[]=input.split("\n|\r");
